36242dc76041da6f5678c4391f9656b177d64b2b
JinhengXie <jinheng.xie@bytedance.com>
2024-08-30 10:45:28 +0800

    update LICENSE

--- inference_mmu.py
+++ inference_mmu.py
@@ -1,3 +1,18 @@
+# coding=utf-8
+# Copyright 2024 NUS Show Lab.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 from PIL import Image
@@ -5,11 +20,10 @@ from tqdm import tqdm
 import numpy as np
 import torch
 import wandb
-from models import Showo, MAGVITv2
+from models import Showo, MAGVITv2, CLIPVisionTower
 from training.prompting_utils import UniversalPrompting, create_attention_mask_for_mmu, create_attention_mask_for_mmu_vit
 from training.utils import get_config, flatten_omega_conf, image_transform
 from transformers import AutoTokenizer
-from models.clip_encoder import CLIPVisionTower
 from transformers import CLIPImageProcessor
 
 import training.conversation as conversation_lib
