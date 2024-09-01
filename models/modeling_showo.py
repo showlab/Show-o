934ea8596a8c015b985f6dc38f15554d1480602e
JinhengXie <jinheng.xie@bytedance.com>
2024-09-01 16:50:39 +0800

    add training loss part

--- models/modeling_showo.py
+++ models/modeling_showo.py
@@ -1,17 +1,24 @@
+# coding=utf-8
+# Copyright 2024 NUS Show Lab, HuggingFace.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 import torch.nn.functional as F
 from transformers import AutoConfig
 from .modeling_utils import ConfigMixin, ModelMixin, register_to_config
 from .sampling import cosine_schedule, mask_by_random_topk
 from .phi import PhiForCausalLM
-
-try:
-    import xformers.ops as xops
-
-    is_xformers_available = True
-except ImportError:
-    is_xformers_available = False
-
 
 class Showo(ModelMixin, ConfigMixin):
     _supports_gradient_checkpointing = True
@@ -25,13 +32,17 @@ class Showo(ModelMixin, ConfigMixin):
             llm_model_path='',
             codebook_size=8192,
             num_vq_tokens=256,
+            load_from_showo=True,
             **kwargs,
     ):
         super().__init__()
         self.vocab_size = vocab_size
         self.register_to_config(mask_token_id=vocab_size - 1)
-        config = AutoConfig.from_pretrained(llm_model_path)
-        self.showo = PhiForCausalLM(config)
+        if load_from_showo:
+            config = AutoConfig.from_pretrained(llm_model_path)
+            self.showo = PhiForCausalLM(config)
+        else:
+            self.showo = PhiForCausalLM.from_pretrained(llm_model_path, attn_implementation='sdpa')
         self.showo.resize_token_embeddings(self.vocab_size)
         self.output_size = self.vocab_size
 
@@ -52,7 +63,10 @@ class Showo(ModelMixin, ConfigMixin):
             attention_mask=None,
             labels=None,
             label_smoothing=0.0,
-            config=None,
+            batch_size_t2i=0,
+            batch_size_lm=0,
+            batch_size_mmu=0,
+            max_seq_length=128,
             labels_mask_text=None,
             labels_mask_image=None,
             **kwargs,
@@ -64,7 +78,26 @@ class Showo(ModelMixin, ConfigMixin):
             logits = self.showo(inputs_embeds=input_embeddings, attention_mask=attention_mask)['logits']
 
         if labels is not None:
-            raise NotImplementedError
+            # 1. Mask token prediction (discrete diffusion) for image generation
+            # Note that, max_seq_length indicates the maximum number of text tokens, maybe a bit confused.
+            loss_t2i = F.cross_entropy(
+                logits[:batch_size_t2i, max_seq_length + 1:].contiguous().view(-1, self.output_size),
+                labels[:batch_size_t2i, max_seq_length + 1:].contiguous().view(-1), ignore_index=-100,
+            )
+
+            # 2. Next token prediction for language modeling
+            loss_lm = F.cross_entropy(
+                logits[batch_size_t2i:batch_size_t2i + batch_size_lm, :-1].contiguous().view(-1, self.output_size),
+                labels[batch_size_t2i:batch_size_t2i + batch_size_lm, 1:].contiguous().view(-1), ignore_index=-100,
+            )
+
+            # 3. Next token prediction for captioning/multimodal understanding
+            loss_mmu = F.cross_entropy(
+                logits[-batch_size_mmu:, :-1].contiguous().view(-1, self.output_size),
+                labels[-batch_size_mmu:, 1:].contiguous().view(-1), ignore_index=-100,
+            )
+
+            return logits, loss_t2i, loss_lm, loss_mmu
 
         return logits
 
@@ -78,7 +111,6 @@ class Showo(ModelMixin, ConfigMixin):
             guidance_scale=0,
             noise_schedule=cosine_schedule,
             generator: torch.Generator = None,
-            uni_prompting=None,
             config=None,
             **kwargs,
     ):
@@ -88,14 +120,15 @@ class Showo(ModelMixin, ConfigMixin):
         """
         # begin with all image token ids masked
         mask_token_id = self.config.mask_token_id
-        seq_len = config.model.showo.num_vq_tokens
+        num_vq_tokens = config.model.showo.num_vq_tokens
+        num_new_special_tokens = config.model.showo.num_new_special_tokens
 
-        input_ids_minus_lm_vocab_size = input_ids[:, -(seq_len + 1):-1].clone()
+        input_ids_minus_lm_vocab_size = input_ids[:, -(num_vq_tokens + 1):-1].clone()
         input_ids_minus_lm_vocab_size = torch.where(input_ids_minus_lm_vocab_size == mask_token_id,
                                                     mask_token_id,
-                                                    input_ids_minus_lm_vocab_size - config.model.showo.llm_vocab_size - 10)
-        # import ipdb
-        # ipdb.set_trace()
+                                                    input_ids_minus_lm_vocab_size - config.model.showo.llm_vocab_size - num_new_special_tokens)
+
+        # for classifier-free guidance
         if uncond_input_ids is not None:
             uncond_prefix = uncond_input_ids[:, :config.dataset.preprocessing.max_seq_length + 1]
 
@@ -106,12 +139,12 @@ class Showo(ModelMixin, ConfigMixin):
                 model_input = torch.cat([input_ids, uncond_input_ids])
                 cond_logits, uncond_logits = self(model_input, attention_mask=attention_mask).chunk(2)
                 # logits = uncond_logits + guidance_scale * (cond_logits - uncond_logits)
-                # it seems that muse has different cfg setting
+                # it seems that muse has a different cfg setting
                 logits = (1 + guidance_scale) * cond_logits - guidance_scale * uncond_logits
-                logits = logits[:, -(seq_len + 1):-1, config.model.showo.llm_vocab_size + 10:-1]
+                logits = logits[:, -(num_vq_tokens + 1):-1, config.model.showo.llm_vocab_size + num_new_special_tokens:-1]
             else:
                 logits = self(input_ids, attention_mask=attention_mask)
-                logits = logits[:, -(seq_len + 1):-1, config.model.showo.llm_vocab_size + 10:-1]
+                logits = logits[:, -(num_vq_tokens + 1):-1, config.model.showo.llm_vocab_size + num_new_special_tokens:-1]
 
             probs = logits.softmax(dim=-1)
             sampled = probs.reshape(-1, logits.size(-1))
@@ -130,7 +163,7 @@ class Showo(ModelMixin, ConfigMixin):
             # Ignores the tokens given in the input by overwriting their confidence.
             selected_probs = torch.where(unknown_map, selected_probs, torch.finfo(selected_probs.dtype).max)
             # Gets mask lens for each sample in the batch according to the mask ratio.
-            mask_len = (seq_len * mask_ratio).floor().unsqueeze(0).to(logits.device)
+            mask_len = (num_vq_tokens * mask_ratio).floor().unsqueeze(0).to(logits.device)
             # Keeps at least one of prediction in this round and also masks out at least
             # one and for the next iteration
             mask_len = torch.max(
@@ -140,8 +173,9 @@ class Showo(ModelMixin, ConfigMixin):
             temperature = temperature * (1.0 - ratio)
             masking = mask_by_random_topk(mask_len, selected_probs, temperature, generator=generator)
             # Masks tokens with lower confidence.
-            input_ids[:, -(seq_len + 1):-1] = torch.where(masking, mask_token_id,
-                                                          sampled_ids + config.model.showo.llm_vocab_size + 10)
+            input_ids[:, -(num_vq_tokens + 1):-1] = torch.where(masking, mask_token_id,
+                                                          sampled_ids + config.model.showo.llm_vocab_size
+                                                          + num_new_special_tokens)
             input_ids_minus_lm_vocab_size = torch.where(masking, mask_token_id, sampled_ids)
 
         return sampled_ids
