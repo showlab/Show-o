bc3385d7041eb91262c7a26e321ba191935805a2
Sierkinhane <sierkinhane@gmail.com>
2025-06-30 15:54:13 +0800

    upload training codes

--- show-o2/train_showo2_1.5b_stage1.sh
+++ show-o2/train_showo2_1.5b_stage1.sh
@@ -3,7 +3,7 @@ export OMP_NUM_THREADS=8
 # training with a relatively large-scale dataset
 accelerate launch --config_file ../accelerate_configs/8_gpus_deepspeed_zero2.yaml --main_process_port=9999 train_stage_one.py config=configs/showo2_1.5b_stage_1_a.yaml;
 
-# change the directory name if you have modified it in your config files
+# change the directory name if you have modified them in your config files
 mkdir show-o2-qwen2-5-2b-stage-1-b;
 cd ./show-o2-qwen2-5-2b-stage-1-b;
 cp -r ../show-o2-qwen2-5-2b-stage-1-a/checkpoint-150000 .;
--- show-o2/train_showo2_1.5b_stage2.sh
+++ show-o2/train_showo2_1.5b_stage2.sh
@@ -1,20 +1,20 @@
 export OMP_NUM_THREADS=8
 
 # we follow the training schedule of LLaVA-OneVision, which includes three sub-stages composing of different instructional tuning data.
-accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_more_v3.py config=configs/showo2_1.5b_stage_2_a.yaml;
+accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_stage_two.py config=configs/showo2_1.5b_stage_2_a.yaml;
 
-# change the directory name if you have modified it in your config files
+# change the directory name if you have modified them in your config files
 mkdir showo2-1.5b-stage-2-b;
 cd ./showo2-1.5b-stage-2-b;
 cp -r ../showo2-1.5b-stage-2-a/checkpoint-final .;
 mv ./checkpoint-final ./checkpoint-0/;
 cd ../;
-accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_more_v3.py config=configs/showo2_1.5b_stage_2_b.yaml;
+accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_stage_two.py config=configs/showo2_1.5b_stage_2_b.yaml;
 
-# change the directory name if you have modified it in your config files
+# change the directory name if you have modified them in your config files
 mkdir showo2-1.5b-stage-2-c;
 cd ./showo2-1.5b-stage-2-c;
 cp -r ../showo2-1.5b-stage-2-b/checkpoint-final .;
 mv ./checkpoint-final ./checkpoint-0/;
 cd ../;
-accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_more_v3.py config=configs/showo2_1.5b_stage_2_c.yaml;
+accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_stage_two.py config=configs/showo2_1.5b_stage_2_c.yaml;
