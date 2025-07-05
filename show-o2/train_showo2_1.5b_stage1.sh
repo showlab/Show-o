d4f94e229b07e5bfb7ae6c1f164beef0a173fa0a
Sierkinhane <sierkinhane@gmail.com>
2025-07-05 20:50:31 +0800

    update

--- show-o2/train_showo2_1.5b_stage1.sh
+++ show-o2/train_showo2_1.5b_stage1.sh
@@ -7,9 +7,9 @@ wget https://huggingface.co/Sierkinhane/pre-distilled_semantic_layers/resolve/ma
 accelerate launch --config_file ../accelerate_configs/8_gpus_deepspeed_zero2.yaml --main_process_port=9999 train_stage_one.py config=configs/showo2_1.5b_stage_1_a.yaml;
 
 # change the directory name if you have modified them in your config files
-mkdir show-o2-qwen2-5-2b-stage-1-b;
-cd ./show-o2-qwen2-5-2b-stage-1-b;
-cp -r ../show-o2-qwen2-5-2b-stage-1-a/checkpoint-150000 .;
+mkdir show-o2-1.5b-stage-1-b;
+cd ./show-o2-1.5b-stage-1-b;
+cp -r ../show-o2-1.5b-stage-1-a/checkpoint-150000 .;
 mv ./checkpoint-150000 ./checkpoint-0/;
 cd ../;
 
