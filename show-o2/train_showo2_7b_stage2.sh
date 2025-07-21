b244ebf7ac083996205f6eb3a7cca279ba93c614
Sierkinhane <sierkinhane@gmail.com>
2025-07-21 15:56:26 +0800

    update

--- show-o2/train_showo2_7b_stage2.sh
+++ show-o2/train_showo2_7b_stage2.sh
@@ -1,5 +1,12 @@
 export OMP_NUM_THREADS=8
 
+# copy pre-trained model
+mkdir showo2-7b-stage-2-a;
+cd ./showo2-7b-stage-2-a;
+cp -r ../showo2-7b-stage-1-b/checkpoint-final .;
+mv ./checkpoint-final ./checkpoint-0/;
+cd ../;
+
 # we follow the training schedule of LLaVA-OneVision, which includes three sub-stages composing of different instructional tuning data.
 accelerate launch --config_file ../show-o/accelerate_configs/multi_nodes_6_deepspeed_zero2/8_gpus_node_0.yaml --main_process_port=8888 train_stage_two.py config=configs/showo2_7b_stage_2_a.yaml;
 
