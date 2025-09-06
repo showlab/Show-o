3e886ff4e5f2f6d22252cf8a1310c98182e26940
Sierkinhane <sierkinhane@gmail.com>
2025-09-06 15:57:37 +0800

    release show-o2 models further fine-tuned on video understanding data

--- show-o2/utils.py
+++ show-o2/utils.py
@@ -7,6 +7,7 @@ import os
 from copy import deepcopy
 from collections import OrderedDict
 import random
+from decord import VideoReader, cpu
 
 ##################################################
 #              config utils
@@ -231,3 +232,23 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+
+def load_video(video_path, max_frames_num, fps, force_sample=False):
+    if max_frames_num == 0:
+        return np.zeros((1, 432, 432, 3))
+
+    vr = VideoReader(video_path, ctx=cpu(0), num_threads=1)
+    total_frame_num = len(vr)
+    video_time = total_frame_num / vr.get_avg_fps()
+    fps = round(vr.get_avg_fps() / fps)
+    frame_idx = [i for i in range(0, len(vr), fps)]
+    frame_time = [i / fps for i in frame_idx]
+    if len(frame_idx) > max_frames_num or force_sample:
+        sample_fps = max_frames_num
+        uniform_sampled_frames = np.linspace(0, total_frame_num - 1, sample_fps, dtype=int)
+        frame_idx = uniform_sampled_frames.tolist()
+        frame_time = [i / vr.get_avg_fps() for i in frame_idx]
+    frame_time = ",".join([f"{i:.2f}s" for i in frame_time])
+    spare_frames = [Image.fromarray(item) for item in vr.get_batch(frame_idx).asnumpy()]
+
+    return spare_frames, frame_time, video_time
